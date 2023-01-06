10c85082ea6118efe9c67490f8ec8247404da5b6
tomatocuke <772532526@qq.com>
2023-01-06 19:18:52 +0800

    first commit

--- scripts/config.js
+++ scripts/config.js
@@ -1,10 +1,11 @@
 export default {
-  author: '', // 作者或者备案号
-  author_site: 'https://beian.miit.gov.cn/#/Integrated/index', // 链接
-  keywords: ['技术博客', 'golang'],
-  title: '我的博客', // 网站标题
+  author: '作者或备案号',
+  author_site: 'http://xxx.com',
+  keywords: [],
+  title: '网站标题',
   description: '',
-  origin: '', // 部署的域名 
+  origin: 'https://<username>.github.io',
   public_path: '',
-  github_repository: '', // 仓库地址
+  github_repository:
+    '', // 仓库地址
 };
