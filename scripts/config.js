22af3b22726167b3a8e23c029dde55564f3ae384
tomatocuke <772532526@qq.com>
2024-02-20 11:05:37 +0800

    opt

--- scripts/config.js
+++ scripts/config.js
@@ -2,11 +2,10 @@ export default {
   author: '浙ICP备2021005082号-3',
   author_site: 'https://beian.miit.gov.cn',
   keywords: ['技术博客', 'golang'],
-  title: '似水流年',
+  title: '某技术博客',
   description: '',
   origin: '',
-  public_path: 'https://kikia.cc', // 你的域名
-  github_repository:
-    'https://github.com/tomatocuke', // 你的仓库
+  public_path: 'https://blog.kikia.cc', // 你的域名
+  github_repository: 'https://github.com/tomatocuke', // 你的仓库
   csdn_repository: 'https://blog.csdn.net/z772532526',
 };
